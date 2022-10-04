bfb6482feddaf027c21c559b921edb03af79e597
Liam B <112952569+LiamB72@users.noreply.github.com>
2022-10-04 09:13:40 +0200

    Update exo_8_bis.py

--- Algo/tp2-complementary/exo_8_bis.py
+++ Algo/tp2-complementary/exo_8_bis.py
@@ -1,19 +1,18 @@
 #Formule des nombre premiers
 from sympy.ntheory import isprime
 
+#Choisi d'une liste de nombres premiers à partir des nombres donnés par l'ultilisateur
 a = int(input("input your first integer number, a = "))
 b = int(input("input your second integer number, b = "))
 print("now the list of the prime numbre in the range you gave")
 
-
-
 while True:
-    if b>a:
+    if b>a: #Si b est supièreur à a alors
         if b > 0 or a < 0 and b < 0:
             for i in range(a,b+1):
                 if isprime(i)==True:
                     print(i)
             break
-    else:
+    else:   #Sinon si b est infièreur à a alors
         a=int(input("input your first integer number, a = "))
         b=int(input("Choose your Final number that it bigger than the first one, b="))
