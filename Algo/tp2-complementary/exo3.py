729c7ac862bd5a7807a77c8dfc6353b636312a86
Liam B <112952569+LiamB72@users.noreply.github.com>
2022-10-14 18:46:09 +0200

    Update exo3.py

--- Algo/tp2-complementary/exo3.py
+++ Algo/tp2-complementary/exo3.py
@@ -1,13 +1,10 @@
-#this exercise is out of my reach of understanding
-
-#this program is bad, thus dont blame me i've tried atleast
-#To the reason of it being bad it's because it's asking :
-#"Create a linear fonction and its variation table in python" while i'm only a 4 week python student like how ;-;
-
 m = int(input("votre coef. directeur est : "))
 p = int(input("votre ordonnée à l'origine est : "))
-x = 10
 
-f = m*x+p
-
-#has to make a variation table but i don't know how
+while True:
+    print("for a range of -10 to 10 :")
+    for x in reversed(range(0, -11, -1)):
+        print(m*x+p, end=", ")
+    for x in range(0, 11):
+        print(m*x+p, end=", ")
+    break
