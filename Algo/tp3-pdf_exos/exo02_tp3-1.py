c79e6331bc07c65ac873777633cd79a27571387d
Liam B <112952569+LiamB72@users.noreply.github.com>
2022-10-14 18:58:43 +0200

    Add files via upload

---
+++ Algo/tp3-pdf_exos/exo02_tp3-1.py
@@ -0,0 +1,7 @@
+def multiplie (a,b): 
+    return a * b 
+ 
+c=multiplie(2,5) 
+print("Résultat: ",c) 
+d=multiplie(4,5) 
+print("Résultat: ",d)
---
+++ Algo/tp3-pdf_exos/exo06_tp3.py
@@ -0,0 +1,8 @@
+def test():
+    b = 5       #variable locale 
+    print(a, b) 
+     
+a = 2          #variable globale 
+b = 7          #variable globale 
+test() 
+print(a, b)
