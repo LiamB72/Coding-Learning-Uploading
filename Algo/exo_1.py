0851522a285881f663f7f0d393b40868d727e40d
Liam B <112952569+LiamB72@users.noreply.github.com>
2022-11-10 15:50:58 +0100

    Add files via upload

---
+++ Algo/exo_1.py
@@ -0,0 +1,12 @@
+def afficheTuple(tuple):
+	prenom,nom,adresse,codePostal,ville=tuple
+	print('prenom :',prenom)
+	print('nom :',nom)
+	print('adresse :',adresse)
+	print('code postal :',codePostal)
+	print('ville :',ville)
+
+
+tuple=('bruce','wayne','3 impasse de la chauve souris',72000,'le mans')
+afficheTuple(tuple)
+
---
+++ Algo/exo_4_simpler.py
@@ -0,0 +1,2 @@
+liste=[20,8,9,2,35,49]
+print('le plus petit nombre de la variable valeur est', min(liste))
