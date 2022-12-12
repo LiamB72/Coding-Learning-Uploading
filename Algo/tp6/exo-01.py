0858a04dbb6e0a0005e714fd438ef69b22c850a8
Liam B <112952569+LiamB72@users.noreply.github.com>
2022-12-12 20:23:06 +0100

    Delete exo-01.py

--- Algo/tp6/exo-01.py
+++
@@ -1,9 +0,0 @@
-def insertionTexte(texte):
-    a = ""
-    for i in range(len(texte)-1):
-        a += texte[i]+'*'
-    a += texte[-1]
-    return a
-
-texte=str(input("Votre txt? "))
-print(insertionTexte(texte))
